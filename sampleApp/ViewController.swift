62a04b4b9919e863c26e9d0d161f0271222406a8
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-09-25 05:27:53 +0900

    足し算サンプルプログラム

--- sampleApp/ViewController.swift
+++ sampleApp/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        //入力
+        let num1 = (textField1.text! as NSString).integerValue
+        let num2 = (textField2.text! as NSString).integerValue
+        //処理
+        let result = num1 + num2
+        //出力
+        label.text = "\(result)"
+    }
+    
 }
 
