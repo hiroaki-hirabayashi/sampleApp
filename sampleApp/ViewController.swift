61cc959727bcd91d2dba5a9e863f094b25264d85
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-09-25 04:58:45 +0900

    Initial Commit

---
+++ sampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleApp
+//
+//  Created by 平林宏淳 on 2020/09/25.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
